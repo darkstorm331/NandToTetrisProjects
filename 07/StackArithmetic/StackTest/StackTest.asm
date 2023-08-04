4260223b9e724807749f97b57b7c9195a530d3e3
Michael Brand <mail@michaelbrand.xyz>
2023-08-04 11:51:00 +0100

    .

--- 07/StackArithmetic/StackTest/StackTest.asm
+++ 07/StackArithmetic/StackTest/StackTest.asm
@@ -14,6 +14,37 @@ A=M
 M=D
 @SP
 M=M+1
+// constant
+@SP
+M=M-1
+A=M
+D=M
+@R13
+M=D
+@SP
+M=M-1
+A=M
+D=M
+@R13
+D=D-M
+@_TRUE_0
+D ; JEQ
+@_FALSE_0
+0 ; JMP
+(_TRUE_0)
+D=-1
+@_END_0
+0 ; JMP
+(_FALSE_0)
+D=0
+@_END_0
+0 ; JMP
+(_END_0)
+@SP
+A=M
+M=D
+@SP
+M=M+1
 // push constant 17
 @17
 D=A
@@ -30,6 +61,37 @@ A=M
 M=D
 @SP
 M=M+1
+// constant
+@SP
+M=M-1
+A=M
+D=M
+@R13
+M=D
+@SP
+M=M-1
+A=M
+D=M
+@R13
+D=D-M
+@_TRUE_1
+D ; JEQ
+@_FALSE_1
+0 ; JMP
+(_TRUE_1)
+D=-1
+@_END_1
+0 ; JMP
+(_FALSE_1)
+D=0
+@_END_1
+0 ; JMP
+(_END_1)
+@SP
+A=M
+M=D
+@SP
+M=M+1
 // push constant 16
 @16
 D=A
@@ -41,6 +103,37 @@ M=M+1
 // push constant 17
 @17
 D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// constant
+@SP
+M=M-1
+A=M
+D=M
+@R13
+M=D
+@SP
+M=M-1
+A=M
+D=M
+@R13
+D=D-M
+@_TRUE_2
+D ; JEQ
+@_FALSE_2
+0 ; JMP
+(_TRUE_2)
+D=-1
+@_END_2
+0 ; JMP
+(_FALSE_2)
+D=0
+@_END_2
+0 ; JMP
+(_END_2)
 @SP
 A=M
 M=D
@@ -62,6 +155,37 @@ A=M
 M=D
 @SP
 M=M+1
+// constant
+@SP
+M=M-1
+A=M
+D=M
+@R13
+M=D
+@SP
+M=M-1
+A=M
+D=M
+@R13
+D=D-M
+@_TRUE_3
+D ; JLT
+@_FALSE_3
+0 ; JMP
+(_TRUE_3)
+D=-1
+@_END_3
+0 ; JMP
+(_FALSE_3)
+D=0
+@_END_3
+0 ; JMP
+(_END_3)
+@SP
+A=M
+M=D
+@SP
+M=M+1
 // push constant 891
 @891
 D=A
@@ -78,6 +202,37 @@ A=M
 M=D
 @SP
 M=M+1
+// constant
+@SP
+M=M-1
+A=M
+D=M
+@R13
+M=D
+@SP
+M=M-1
+A=M
+D=M
+@R13
+D=D-M
+@_TRUE_4
+D ; JLT
+@_FALSE_4
+0 ; JMP
+(_TRUE_4)
+D=-1
+@_END_4
+0 ; JMP
+(_FALSE_4)
+D=0
+@_END_4
+0 ; JMP
+(_END_4)
+@SP
+A=M
+M=D
+@SP
+M=M+1
 // push constant 891
 @891
 D=A
@@ -89,6 +244,37 @@ M=M+1
 // push constant 891
 @891
 D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// constant
+@SP
+M=M-1
+A=M
+D=M
+@R13
+M=D
+@SP
+M=M-1
+A=M
+D=M
+@R13
+D=D-M
+@_TRUE_5
+D ; JLT
+@_FALSE_5
+0 ; JMP
+(_TRUE_5)
+D=-1
+@_END_5
+0 ; JMP
+(_FALSE_5)
+D=0
+@_END_5
+0 ; JMP
+(_END_5)
 @SP
 A=M
 M=D
@@ -110,6 +296,37 @@ A=M
 M=D
 @SP
 M=M+1
+// constant
+@SP
+M=M-1
+A=M
+D=M
+@R13
+M=D
+@SP
+M=M-1
+A=M
+D=M
+@R13
+D=D-M
+@_TRUE_6
+D ; JGT
+@_FALSE_6
+0 ; JMP
+(_TRUE_6)
+D=-1
+@_END_6
+0 ; JMP
+(_FALSE_6)
+D=0
+@_END_6
+0 ; JMP
+(_END_6)
+@SP
+A=M
+M=D
+@SP
+M=M+1
 // push constant 32766
 @32766
 D=A
@@ -126,6 +343,37 @@ A=M
 M=D
 @SP
 M=M+1
+// constant
+@SP
+M=M-1
+A=M
+D=M
+@R13
+M=D
+@SP
+M=M-1
+A=M
+D=M
+@R13
+D=D-M
+@_TRUE_7
+D ; JGT
+@_FALSE_7
+0 ; JMP
+(_TRUE_7)
+D=-1
+@_END_7
+0 ; JMP
+(_FALSE_7)
+D=0
+@_END_7
+0 ; JMP
+(_END_7)
+@SP
+A=M
+M=D
+@SP
+M=M+1
 // push constant 32766
 @32766
 D=A
@@ -137,6 +385,37 @@ M=M+1
 // push constant 32766
 @32766
 D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// constant
+@SP
+M=M-1
+A=M
+D=M
+@R13
+M=D
+@SP
+M=M-1
+A=M
+D=M
+@R13
+D=D-M
+@_TRUE_8
+D ; JGT
+@_FALSE_8
+0 ; JMP
+(_TRUE_8)
+D=-1
+@_END_8
+0 ; JMP
+(_FALSE_8)
+D=0
+@_END_8
+0 ; JMP
+(_END_8)
 @SP
 A=M
 M=D
@@ -166,9 +445,71 @@ A=M
 M=D
 @SP
 M=M+1
+// constant
+@SP
+M=M-1
+A=M
+D=M
+@R13
+M=D
+@SP
+M=M-1
+A=M
+D=M
+@R13
+D=D+M
+@SP
+A=M
+M=D
+@SP
+M=M+1
 // push constant 112
 @112
 D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// constant
+@SP
+M=M-1
+A=M
+D=M
+@R13
+M=D
+@SP
+M=M-1
+A=M
+D=M
+@R13
+D=D-M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// constant
+@SP
+M=M-1
+A=M
+D=D-M
+M=D
+@SP
+M=M+1
+// constant
+@SP
+M=M-1
+A=M
+D=M
+@R13
+M=D
+@SP
+M=M-1
+A=M
+D=M
+@R13
+D=D&M
 @SP
 A=M
 M=D
@@ -179,6 +520,32 @@ M=M+1
 D=A
 @SP
 A=M
+M=D
+@SP
+M=M+1
+// constant
+@SP
+M=M-1
+A=M
+D=M
+@R13
+M=D
+@SP
+M=M-1
+A=M
+D=M
+@R13
+D=D|M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// constant
+@SP
+M=M-1
+A=M
+D=D!M
 M=D
 @SP
 M=M+1
